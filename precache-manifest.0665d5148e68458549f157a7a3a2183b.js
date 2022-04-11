77396314b2eb7f7ecbcfe068e1343734c0372550
Mohamed Ajermoune <majermou@e1r12p1.1337.ma>
2022-04-11 12:50:54 +0000

    Updates

---
+++ precache-manifest.0665d5148e68458549f157a7a3a2183b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4799b250af4fb1ffdd711dbc2cf1b60a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82fd31e1f3ad4a694510",
+    "url": "/static/css/main.03f4bb49.chunk.css"
+  },
+  {
+    "revision": "0ac3975264ab11f8232d",
+    "url": "/static/js/2.5969d403.chunk.js"
+  },
+  {
+    "revision": "82fd31e1f3ad4a694510",
+    "url": "/static/js/main.3c5b59ff.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
